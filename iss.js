7cdde3844cdb6bb3e9fda0b765eb8357514eb83f
Kevin Lee <jhssttj@hotmail.com>
2022-09-29 16:20:05 -0600

    Added IP tracker function

---
+++ iss.js
@@ -0,0 +1,29 @@
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) {
+  const request = require('request');
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    const ipData = JSON.parse(body);
+    if (error) {
+      callback(error,null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    callback(null, ipData.ip);
+    return;
+  });
+};
+
+module.exports = { fetchMyIP };
